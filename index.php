77e860dd3dd912abd0c7dbac293326275a3589ef
Corey Nagel <cnagel85@hotmail.com>
2014-01-26 20:43:40 -0700

    getting heroku info

--- index.php
+++ index.php
@@ -1,28 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<link rel="stylesheet" type="text/css" href="includes/style.css" media="screen" />
-	<!--[if lt IE 9]>
-	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-	<title>Corey Elliot Nagel</title>
-</head>
-	<body>
-		<?php 
-		include ('includes/includes.php');
-		include('includes/header.php');
-		$blogPosts = GetBlogPosts();
-		?>
-		<div class="body"> 
-			<div class="main">  
-    			<?php include('includes/posts.php'); ?>	
-    		</div> 
-    		<section class="sidebar">
-    			<?php include('includes/sidebar.php'); ?>
-    		</section> 
-		</div> 
-		<?php include ('includes/footer.php'); ?>
-	</body>
-</html>
-
-
+<?php
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+function pg_connection_string() {
+  return "dbname=abcdefg host=****.amazonaws.com port=5432 user=**** password=**** sslmode=require";
+}
+ 
+# Establish db connection
+$db = pg_connect(pg_connection_string());
+if (!$db) {
+   echo "Database connection error."
+   exit;
+}
+ 
+$result = pg_query($db, "SELECT statement goes here");
+?>
